eff9db0fd77778b29f1bb870baa1f11f349c3c0b
purini-to <pr.hr.urainkyo@gmail.com>
2020-05-18 15:42:21 +0900

    feat: add guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,43 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        guess = guess.trim().to_lowercase();
+        if guess == "quit" || guess == "exit" {
+            break;
+        }
+
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Less => println!("Too small!"),
+            Greater => println!("Too big!"),
+            Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
